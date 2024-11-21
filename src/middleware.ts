5c8907681e425e61f38051a0b7e96ccf3a5604a6
pilif139 <pifilif@gmail.com>
2024-11-21 18:49:20 +0100

    deleted unecessary session verification even when the route wasnt protected

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,17 @@
 import {verifySession} from "@/auth/session";
 import {NextRequest, NextResponse} from "next/server";
-import {cookies} from "next/headers";
-
 const protectedRoutes = ['/dashboard'];
 
 export default async function middleware(req: NextRequest){
   const path = req.nextUrl.pathname;
   const isProtected = protectedRoutes.includes(path);
 
-  const session = await verifySession()
-
-  if(isProtected && !session?.userId) {
-    return NextResponse.redirect(new URL('/login', req.url))
+  if(isProtected) {
+    // Verify the session only if the route is protected to avoid unnecessary calls
+    const session = await verifySession();
+    if(!session.userId){
+      return NextResponse.redirect(new URL('/login', req.url))
+    }
   }
   return NextResponse.next()
 }
