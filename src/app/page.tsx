853cac0e6b26a9563c128c8a71efc1b8ec6468d3
pilif139 <pifilif@gmail.com>
2024-12-05 18:33:30 +0100

    refactored Form component to be more generic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,102 @@
 'use client'
 
-import Form from "@/components/Form";
-import {useEffect, useState} from "react";
+import Form, {FormInput} from "@/components/Form";
+import {useEffect, useRef, useState} from "react";
 import ShortenedLink from "@/components/ShortenedLink";
 import {RiseLoader} from "react-spinners";
+import {addUrl} from "@/actions/actions";
+import {z} from "zod";
+
+const urlSchema = z.object({
+    url: z.string()
+        .url({message: "Invalid URL!"}),
+    alias: z.string()
+        .max(20, {message: "To long alias!"})
+        .optional()
+})
 
 export default function Home() {
-  const [alias, setAlias] = useState<string | undefined>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [spinnerTheme, setSpinnerTheme] = useState<"black" | "white">("black");
+    const [alias, setAlias] = useState<string | undefined>("");
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [spinnerTheme, setSpinnerTheme] = useState<"black" | "white">("black");
+    const urlRef = useRef<HTMLInputElement>(null);
+    const aliasRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-     setSpinnerTheme(window.localStorage.getItem("theme") === "dark" ? "white" : "black");
-  }, []);
+    const [urlError, setUrlError] = useState<string | null>("");
+    const [aliasError, setAliasError] = useState<string | null>("");
 
-  return (
-    <main className="flex flex-col items-center justify-center text-2xl">
-      <Form setAlias={setAlias} setIsLoading={setIsLoading}></Form>
-      {!isLoading && alias && <ShortenedLink alias={alias}/>}
-      <RiseLoader loading={isLoading} size={20} className="mt-20" color={spinnerTheme}/>
-    </main>
-  );
+    const inputs = [
+        {
+            placeholder: "Enter your url here...",
+            type: "text",
+            name: "url",
+            ref: urlRef,
+            animation: {
+                initial: {opacity: 0, x: -200},
+                animate: {opacity: 1, x: 0},
+                transition: {duration: 0.2, type: "tween", stiffness: 500, damping: 20},
+            },
+            error: null,
+            setError: setUrlError,
+        },
+        {
+            placeholder: "Your optional alias...",
+            type: "text",
+            name: "alias",
+            ref: aliasRef,
+            animation: {
+                initial: {opacity: 0, x: 200},
+                animate: {opacity: 1, x: 0},
+                transition: {duration: 0.2, type: "tween", stiffness: 500, damping: 20},
+            },
+            error: null,
+            setError: setAliasError,
+        }
+    ] as FormInput[];
+
+    const theme = window.localStorage.getItem("theme");
+    useEffect(() => {
+        setSpinnerTheme(theme === "dark" ? "white" : "black");
+    }, [theme]);
+
+    const action = async (formData: FormData) => {
+        const newUrl = {
+            url: formData.get("url") as string,
+            alias: formData.get("alias") as string
+        }
+
+        const result = urlSchema.safeParse(newUrl);
+        if (!result.success) {
+            setUrlError(result.error.errors[0].message);
+            setAlias(undefined)
+            if (result.error.errors[1]) {
+                setAliasError(result.error.errors[1].message);
+            }
+            setIsLoading(false);
+            return;
+        }
+
+        // code when the zod validation passes
+        const response = await addUrl(formData)
+        setAlias(response.alias)
+        if (response.error) {
+            setAliasError(response.error)
+        } else {
+            setUrlError("");
+            setAliasError("");
+            if (urlRef.current && aliasRef.current) {
+                urlRef.current.value = "";
+                aliasRef.current.value = "";
+            }
+        }
+        setIsLoading(false);
+    }
+
+    return (
+        <main className="flex flex-col items-center justify-center text-2xl">
+            <Form action={action} inputs={inputs} setIsLoading={setIsLoading}></Form>
+            {!isLoading && alias && <ShortenedLink alias={alias}/>}
+            <RiseLoader loading={isLoading} size={20} className="mt-20" color={spinnerTheme}/>
+        </main>
+    );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,96 +1,87 @@
-"use client"
-
-import {useState, useRef} from "react";
-import {addUrl} from "@/actions/actions";
+import {useState, useRef, SetStateAction, Dispatch, RefObject} from "react";
 import {motion} from "framer-motion";
-import {z} from "zod";
 
-const urlSchema = z.object({
-  url: z.string()
-      .url({message: "Invalid URL!"}),
-  alias: z.string()
-      .max(20, {message: "To long alias!"})
-      .optional()
+type inputType =
+    "text"
+    | "password"
+    | "email"
+    | "number"
+    | "tel"
+    | "range"
+    | "url"
+    | "search"
+    | "date"
+    | "time"
+    | "datetime-local"
+    | "month"
+    | "week"
+    | "color"
+
+const defaultAnimation = {
+    initial: { opacity: 0, x: 0 },
+    animate: { opacity: 1, x: 0 },
+    transition: { duration: 0.2, type: "tween", stiffness: 500, damping: 20 }
+};
+
+export type FormInput = {
+        label?: string,
+        placeholder?: string,
+        type: inputType,
+        name: string,
+        ref?: RefObject<HTMLInputElement>,
+        className?: string,
+        animation?: {
+            initial?: { opacity: number, x: number },
+            animate?: { opacity: number, x: number },
+            transition?: { duration: number, type: string, stiffness: number, damping: number }
+        },
+    }
+    & ({ label: string } | { placeholder: string })
+    & ({
+    error?: string | null
+    setError?: Dispatch<SetStateAction<string | null>>
 })
 
 type FormProps = {
-  setAlias: (alias: string | undefined) => void
-    setIsLoading: (isLoading: boolean) => void
+    inputs: FormInput[],
+    action: (formData: FormData) => void,
+    setIsLoading: Dispatch<SetStateAction<boolean>>
 }
 
-export default function Form({setAlias, setIsLoading} : FormProps){
-  const [urlError, setUrlError] = useState<string>("");
-  const [aliasError, setAliasError] = useState<string>("");
-
-  const urlRef = useRef<HTMLInputElement>(null);
-  const aliasRef = useRef<HTMLInputElement>(null);
-
-  const clientAction = async (formData : FormData) => {
-    const newUrl = {
-      url: formData.get("url") as string,
-      alias: formData.get("alias") as string
-    }
-
-    const result = urlSchema.safeParse(newUrl);
-    if(!result.success){
-      setUrlError(result.error.errors[0].message);
-      setAlias(undefined)
-      if(result.error.errors[1]) {
-        setAliasError(result.error.errors[1].message);
-      }
-      setIsLoading(false);
-      return;
-    }
-
-    // code when the zod validation passes
-    const response = await addUrl(formData)
-    setAlias(response.alias)
-    if(response.error){
-        setAliasError(response.error)
-    }
-    else{
-      setUrlError("");
-      setAliasError("");
-      if(urlRef.current && aliasRef.current) {
-        urlRef.current.value = "";
-        aliasRef.current.value = "";
-      }
-    }
-    setIsLoading(false);
-  }
-
-  return (
-        <form action={clientAction} className="flex flex-col items-center gap-5">
-          <motion.input
-                  initial={{opacity: 0, x: -200}}
-                  animate={{opacity: 1, x: 0}}
-                  transition={{duration: 0.2, type: "tween", stiffness: 500, damping: 20}}
-                  type="text"
-                  placeholder="Enter your url here..."
-                  className="rounded-2xl p-2 outline-none transition w-[15em] focus:bg-slate-100 dark:placeholder:text-slate-800 dark:bg-slate-400 dark:text-black md:w-[30em] dark:focus:bg-slate-300"
-                  name="url"
-                  ref={urlRef}
-          />
-          {urlError && <motion.p initial={{y:-20}} animate={{y:0}} className="text-red-500">{urlError}</motion.p>}
-          <motion.input
-                  initial={{opacity: 0, x: 200}}
-                  animate={{opacity: 1, x: 0}}
-                  transition={{duration: 0.2, type: "tween", stiffness: 500, damping: 20}}
-                  type="text"
-                  placeholder="Your optional alias..."
-                  className="rounded-2xl p-2 outline-none transition w-[15em] focus:bg-slate-100 dark:placeholder:text-slate-800 dark:bg-slate-400 dark:text-black md:w-[30em] dark:focus:bg-slate-300"
-                  name="alias"
-                  ref={aliasRef}
-          />
-          {aliasError && <motion.p initial={{y:-20}} animate={{y:0}} className="text-red-500">{aliasError}</motion.p>}
-          <motion.button
-                  initial={{opacity: 0, scale: 0.9}}
-                  animate={{opacity: 1, scale: 1}}
-                  transition={{duration: 0.4, type: "tween", stiffness: 500, damping: 20}}
-                  type="submit"
-                  onClick={() => setIsLoading(true)}
-                  className="mx-4 w-max rounded-2xl bg-violet-500 p-2 px-8 text-white transition hover:bg-violet-600">Submit
-          </motion.button>
+export default function Form({inputs, action, setIsLoading}: FormProps) {
+    return (
+        <form action={action} className="flex flex-col items-center gap-5">
+            {
+                inputs.map(({label, placeholder, type, name, ref, className, animation = defaultAnimation, error, setError}) => (
+                    <>
+                        <motion.div
+                            key={name}
+                            initial={animation?.initial}
+                            animate={animation?.animate}
+                            transition={animation?.transition}
+                            className="flex flex-col items-center gap-1">
+                            {label && <label htmlFor={name}>{label}</label>}
+                            <input
+                                ref={ref}
+                                type={type}
+                                name={name}
+                                id={name}
+                                placeholder={placeholder}
+                                className={`${className} rounded-2xl p-2 outline-none transition w-[15em] focus:bg-slate-100 dark:placeholder:text-slate-800 dark:bg-slate-400 dark:text-black md:w-[30em] dark:focus:bg-slate-300 ${error ? "border-red-500" : ""}`}
+                            />
+                        </motion.div>
+                        {error && <p className="text-red-500">{error}</p>}
+                    </>
+                ))
+            }
+            <motion.button
+                initial={{opacity: 0, scale: 0.9}}
+                animate={{opacity: 1, scale: 1}}
+                transition={{duration: 0.4, type: "tween", stiffness: 500, damping: 20}}
+                type="submit"
+                onClick={() => setIsLoading(true)}
+                className="mx-4 w-max rounded-2xl bg-violet-500 p-2 px-8 text-white transition hover:bg-violet-600">Submit
+            </motion.button>
         </form>
-  )
+    )
 }
